9f4dd334982ee01965fd45c96412fe23911c80ce
MinYong Lee <glakffkdldk1@gmail.com>
2023-04-16 15:04:42 +0900

    Add files via upload

---
+++ Lab2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cout << "마름모를 구성할 숫자를 입력해주세요: ";
+  cin >> num;
+
+  cout << "  " <<  num  << endl;
+  cout << " " << num << num << num << endl;
+  cout << num << num << num << num << num << endl;
+  cout << " " << num << num << num << endl;
+  cout << "  " <<  num  << endl;
+}
