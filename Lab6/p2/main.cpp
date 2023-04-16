337a33c72591df4b8322dc8694913ba19a3e44c3
MinYong Lee <glakffkdldk1@gmail.com>
2023-04-16 16:14:17 +0900

    Add files via upload

---
+++ Lab6/p2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int (&arr)[20], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << i + 1 << " 번: " << arr[i]  << endl;
+  }
+}
+
+void initializeArray(int (&arr)[20], int size) {
+  srand((unsigned int)time(0));
+  for (int i = 0; i < size; i++) {
+    arr[i] = rand() % 100;
+  }
+}
+
+void sortArray(int (&arr)[20], int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size-1-i; j++) {
+      if (arr[j] > arr[j+1]) {
+        int count = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = count;
+      }
+    }
+  }
+  printArray(arr,size);
+}
+
+int main() {
+  int arr[20];
+  int size = sizeof(arr)/sizeof(int);
+
+  initializeArray(arr,size);
+  sortArray(arr, size);
+}
